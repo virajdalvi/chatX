f03751455c1c9ac39656f5056bcb26e93c7e5ca1
Viraj Dalvi <59591172+virajdalvi@users.noreply.github.com>
2022-08-10 18:50:27 +0530

    MyChats development starts from here

--- frontend/src/components/miscellaneous/MyChats.js
+++ frontend/src/components/miscellaneous/MyChats.js
@@ -1,8 +1,27 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 import { ChatState } from '../../Context/ChatProvider'
+import { useToast, } from '@chakra-ui/react'
+
 const MyChats = () => {
   const[loggedUser,setLoggedUser]=useState()
   const {selectedChat,user,setSelectedChat,chats,setChats} = ChatState()
+  const toast = useToast()
+  const fetchChats = async ()=>{
+    try{
+      const config = {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+      const { data } = await axios.get("/api/chat",config);
+      setChats(data)
+    }
+    catch(error){
+      
+
+    }
+  }
   return (
     <div>MyChats</div>
   )
